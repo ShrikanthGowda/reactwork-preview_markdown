02d67a69e8ee4d23d70ad3d01aef40b7ca408c4f
glo66847 <glo66847@adobe.com>
2020-08-19 18:32:43 +0530

    stylesheet design

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import editMarkdown from '../actions';
 import { connect } from 'react-redux';
+import '../stylesheets/style.css';
 
 class App extends Component {
     
     render() {
         return (
             <div>
-                <h1>{ this.props.message }</h1>
-            </div>          
+                <div className="split left">
+                    <div className="centered">
+                        <form>
+                            <label for="editor">Editor</label>
+                            <textarea id="editor" name="editorMsg" rows="40" cols="100">
+                                {this.props.message}
+                            </textarea>
+                            <input type="submit"/>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="split right">
+                    <div className="centered">
+                        <form>
+                            <label for="preview">Preview</label>
+                            <textarea id="preview" name="previewMsg" rows="40" cols="100" readonly>
+                                {this.props.message}
+                            </textarea>
+                            <input type="submit"></input>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         );
     }
 }
